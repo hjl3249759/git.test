efde846f9f5a3c426f9e5976f0371784ad985356
hejinlong <hejinlong@cast.cn>
2019-06-13 10:28:15 +0800

    将class1添加到本地仓库中

---
+++ git_test/src/cn/itcast/git/Class1.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+public class Class1 {
+
+    public static void main(String[] args) {
+        System.out.println("git......");
+        System.out.println("git......");
+        System.out.println("git......");
+        System.out.println("git......");
+        System.out.println("git......");
+    }
+}
